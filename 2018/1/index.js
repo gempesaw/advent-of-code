def958541194b149c2014a7bb6797313df05cf14
Daniel Gempesaw <gempesaw@gmail.com>
2018-12-10 00:54:42 -0500

    Finish the second half of day 1

--- 2018/1/index.js
+++ 2018/1/index.js
@@ -1,9 +1,38 @@
 const fs = require('fs');
+const assert = require('assert');
+
+const getRepeated = (input) => (acc = [ 0 ], argFirst = null, it = 0) => {
+  process.stdout.write(`${it}\r`);
+  if (argFirst !== null) {
+    return argFirst;
+  }
+
+  const { acc: nextAcc, first: nextFirst } = input
+    .map(Number)
+    .reduce(({ acc: list, first }, it) => {
+      const next = list[list.length - 1] + it;
+      const nextList = list.concat(next);
+
+      if (first === null && list.includes(next)) {
+        return { acc: nextList, first: next };
+      }
+
+      return { acc: nextList, first };
+    }, { acc, first: null });
+
+  return getRepeated(input)(nextAcc, nextFirst, ++it);
+};
 
 try {
   const input = fs.readFileSync(`${__dirname}/input`, 'utf-8').split('\n');
 
-  console.log(input.map(Number).reduce((a, b) => a + b));
+  assert.equal(getRepeated([ +1, -1 ])(), 0);
+  assert.equal(getRepeated([ +3, +3, +4, -2, -4 ])(), 10);
+  assert.equal(getRepeated([ -6, +3, +8, +5, -6 ])(), 5);
+  assert.equal(getRepeated([ +7, +7, -2, -7, -4 ])(), 14);
+
+  console.log(JSON.stringify(getRepeated(input)(), null, 2));
+
 }
 catch (err) {
   console.log(err);
