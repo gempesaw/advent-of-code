f8ca81da3f8646c55fa677cd8fd8b44d487d2a31
Daniel Gempesaw <gempesaw@gmail.com>
2018-12-17 18:45:25 -0500

    Finish d7p2: parallel workers! (arrays galore)

--- 2018/7/index.js
+++ 2018/7/index.js
@@ -24,17 +24,30 @@ const steps = Object.entries(input.split('\n').filter(Boolean).reduce((steps, li
   .map(([step, prereq]) => ({ step, prereq }));
 
 const getNext = (steps) => {
-  const next = steps.filter(({ prereq }) => prereq.length === 0);
+  if (steps.some(({ duration }) => duration)) {
+    const working = steps.filter(({ duration, initialDuration }) => duration < initialDuration);
+    const ready = steps
+      .filter(({
+        prereq,
+        duration,
+        initialDuration
+      }) => prereq.length === 0 && duration === initialDuration);
 
-  return next.sort(({ step: a }, { step: b }) => a < b ? -1 : a > b ? 1 : 0)[0];
+    return [ ...working, ...ready ];
+  }
+
+  return steps.filter(({ prereq }) => prereq.length === 0);
 };
+
+const getNextSingle = (steps) => getNext(steps)
+  .sort(({ step: a }, { step: b }) => a < b ? -1 : a > b ? 1 : 0)[0];
 
 const order = (done = '', steps) => {
   if (steps.length === 0) {
     return done;
   }
 
-  const { step: nextStep } = getNext(steps);
+  const { step: nextStep } = getNextSingle(steps);
 
   return order(`${done}${nextStep}`, steps
     .filter(({ step }) => step !== nextStep)
@@ -44,4 +57,49 @@ const order = (done = '', steps) => {
     })));
 };
 
-console.log(order('', steps));
+const stepsWithDuration = (steps) => steps.map(({ step, ...rest }) => ({
+  step,
+  ...rest,
+  duration: step.charCodeAt(0) - 4,
+  initialDuration: step.charCodeAt(0) - 4
+}));
+
+const asRegex = (steps) => new RegExp(`[${steps.map(({ step }) => step).join('')}]`, 'g');
+
+const orderWithDuration = ({ workers }) => ({ done = '', p2steps = [], time = 0 }) => {
+  if (p2steps.length === 0) {
+    return console.log(JSON.stringify({ done, time: time - 1 }, null, 2));
+  }
+
+  const finishedSteps = p2steps.filter(({ duration }) => duration === 0);
+  const nextStepsAfterFinished = p2steps
+    .filter(({ duration }) => duration > 0)
+    .map(({ step, prereq, ...rest }) => ({
+      step,
+      prereq: prereq.replace(asRegex(finishedSteps), ''),
+      ...rest
+    }));
+
+  const ticked = getNext(nextStepsAfterFinished)
+    .slice(0, workers)
+    .map(({ duration, ...rest }) => ({
+      ...rest,
+      duration: --duration
+    }));
+
+  const nextSteps = nextStepsAfterFinished
+    .filter(({ step }) => !ticked.some(({ step: tickedStep }) => tickedStep === step))
+    .concat(ticked);
+
+  debugger;
+  return orderWithDuration({ workers })({
+    done: `${done}${finishedSteps.map(({ step }) => step).sort()}`,
+    p2steps: nextSteps,
+    time: ++time
+  });
+};
+
+const p2steps = stepsWithDuration(steps);
+orderWithDuration({ workers: 5 })({ p2steps });
+
+// console.log(`1: ${order('', steps)}`);
