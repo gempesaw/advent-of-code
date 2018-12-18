c2a945330485fdc487c5b274dbf842e05f15ccdd
Daniel Gempesaw <gempesaw@gmail.com>
2018-12-18 10:26:58 -0500

    (d8p1, d8p2) again! this time with immutability :D

--- 2018/8/index.js
+++ 2018/8/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const string = fs.readFileSync(`${__dirname}/input`, 'utf-8');
 // const string = '2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2';
+// const string = '1 1 0 1 8 9';
 const input = string.split(' ').map(Number);
 
 const parse = (input, node = { children: [], metadata: [] }, remainingChildren = 0) => {
@@ -18,6 +19,47 @@ const parse = (input, node = { children: [], metadata: [] }, remainingChildren =
   rest = rest.slice(metadataCount);
 
   return [ node, rest ];
+};
+
+const parseImmutable = ({
+  input = [],
+  node = { children: [], metadata: [] },
+  remainingChildren = 0,
+  remainingMetadata = 0
+}) => {
+  // console.log({ input, node, remainingChildren, remainingMetadata });
+  if (remainingChildren > 0) {
+    const [ child, rest ] = parseImmutable({ input, node });
+
+    return parseImmutable({
+      input: rest,
+      node: { children: node.children.concat(child), metadata: [] },
+      remainingChildren: remainingChildren - 1,
+      remainingMetadata
+    });
+  }
+
+  if (remainingMetadata > 0) {
+    const metadata = input.slice(0, remainingMetadata);
+    const rest = input.slice(remainingMetadata);
+
+    return [
+      { ...node, metadata },
+      rest
+    ];
+  }
+
+  const [ childCount, metadataCount, ...rest ] = input;
+
+  if (!rest.length) {
+    return [ node, input ];
+  }
+
+  return parseImmutable({
+    input: rest,
+    remainingChildren: childCount,
+    remainingMetadata: metadataCount
+  });
 };
 
 const getNodes = ({ children = [], metadata = [] }, acc = []) => {
@@ -40,9 +82,12 @@ const getValue = ({ children = [], metadata = [] }) => {
   return childNodes.reduce((acc, node) => acc + getValue(node), 0);
 };
 
-const [ tree ] = parse(input);
+// const [ tree ] = parse(input);
+const [ tree ] = parseImmutable({ input });
 const nodes = getNodes(tree);
 const metadata = nodes.reduce((acc, { metadata }) => acc.concat(metadata), []);
 
 console.log(`1: ${metadata.reduce((a, b) => a + b, 0)}`);
 console.log(`2: ${getValue(tree)}`);
+// 1: 41760
+// 2: 25737
