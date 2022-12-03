dacaabec6e5a90ab8db2f69187948bf978e6c9cb
Daniel Gempesaw <gempesaw@gmail.com>
2022-12-03 00:35:14 -0500

    d3 the mighty ducks

---
+++ 2022/03/03.el
@@ -0,0 +1,52 @@
+(progn
+  (defun aoc-s-split (separator string)
+    (s-split separator string t))
+
+  (defun aoc-to-priority (char)
+    (let ((priority (- (string-to-char char) 96)))
+      (if (> priority 0)
+          priority
+        (+ priority 58))))
+
+  (defun aoc-find-badge (elves)
+    (let (
+          (first (aoc-s-split "" (car elves)))
+          (second (aoc-s-split "" (cadr elves)))
+          (third (aoc-s-split "" (caddr elves))))
+      (--first (and (-contains? second it)
+                    (-contains? third it))
+               first)))
+
+
+  (->> "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+"
+       (aoc-s-split "\n")
+       (-partition 3)
+       (-map 'aoc-find-badge)
+       (-map 'aoc-to-priority)
+       (-sum)))
+
+
+;;; one
+(--> (f-read-text "data")
+     (s-split "\n" it t)
+     (--map (let* ((halves (-split-at (/ (length it) 2) (s-split "" it t)))
+                   (first (car halves))
+                   (second (cadr halves)))
+              (--first (-contains? second it) first))
+            it)
+     (-map 'aoc-to-priority it)
+     (-sum it))
+
+;;; two
+(->> (f-read-text "data")
+     (aoc-s-split "\n")
+     (-partition 3)
+     (-map 'aoc-find-badge)
+     (-map 'aoc-to-priority)
+     (-sum))
