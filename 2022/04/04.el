ba5fc97e0cefc166c2b9bb3bdd975cfc31b19784
Daniel Gempesaw <gempesaw@gmail.com>
2022-12-04 00:13:26 -0500

    one two three foUR

---
+++ 2022/04/04.el
@@ -0,0 +1,51 @@
+
+(->> "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+"
+     (aoc-s-split "\n")
+     (--map (->> it
+                 (s-split ",")
+                 (--map (->> it
+                             (s-split "-")
+                             (-map 'string-to-number)
+                             (apply 'number-sequence)))))
+     (--count (let* ((first (car it))
+                     (second (cadr it))
+                     (intersection (-intersection first second)))
+                (or (-same-items? first intersection)
+                    (-same-items? second intersection))))
+     )
+
+
+;;; one
+(->> (aoc-data-as-lines)
+     (--map (->> it
+                 (s-split ",")
+                 (--map (->> it
+                             (s-split "-")
+                             (-map 'string-to-number)
+                             (apply 'number-sequence)))))
+     (--count (let* ((first (car it))
+                     (second (cadr it))
+                     (intersection (-intersection first second)))
+                (or (-same-items? first intersection)
+                    (-same-items? second intersection))))
+     )
+
+;;; two
+(->> (aoc-data-as-lines)
+     (--map (->> it
+                 (s-split ",")
+                 (--map (->> it
+                             (s-split "-")
+                             (-map 'string-to-number)
+                             (apply 'number-sequence)))))
+     (--count (let* ((first (car it))
+                     (second (cadr it))
+                     (intersection (-intersection first second)))
+                intersection))
+     )
